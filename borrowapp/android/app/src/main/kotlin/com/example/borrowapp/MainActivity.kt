e279e82aa11ab51d7755e1dd52848f383cef3738
Nareerak <6531601068@lamduan.mfu.ac.th>
2024-10-30 14:25:57 +0700

    Initial commit

---
+++ borrowapp/android/app/src/main/kotlin/com/example/borrowapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.borrowapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ borrowapp/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:borrowapp/edit.dart';
+import 'package:borrowapp/request.dart';
+import 'package:flutter/material.dart';
+import 'package:borrowapp/add.dart';
+
+
+void main() => runApp(
+      MaterialApp(
+        home: BorrowBookAppRequest(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+    
